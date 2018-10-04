8a898c1ecdd9bb41a2666e905a8e464c14125bac
Hubert Pompecki <hpompecki@gmail.com>
2018-10-04 10:49:18 +0100

    Fix favicon href (#209)

--- packages/reference/components/CustomHead.js
+++ packages/reference/components/CustomHead.js
@@ -6,7 +6,7 @@ class CustomHead extends Component {
     return <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <script src='https://js.stripe.com/v3/' />
-      <link rel='icon' type='image/png' sizes='32x32' href='../../static/favicon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon.png' />
     </Head>
   }
 }
