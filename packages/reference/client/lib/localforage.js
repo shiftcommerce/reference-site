5e1efef3b7cf4c735f063d962e28b0920d92b16f
Callum Barratt <callum@barratt.me>
2018-10-23 10:13:29 +0100

    Catch localforage initialisation (#251)
    * Catch localforage initialisation
    
    * Use arrow syntax
    
    * Update messages
    
    * Remove extra space
    
    * Disable eslint
    
    * Dont log the storage error

--- packages/reference/client/lib/localforage.js
+++ packages/reference/client/lib/localforage.js
@@ -13,6 +13,23 @@ class LocalForage {
       driver: localforage.INDEXEDDB,
       name: 'CART'
     })
+
+    /* eslint-disable */
+    // This block has purely been added to stop
+    // unhandled promises occuring in the test suite
+    localforage.ready().then(() => {
+      // This code runs once localforage
+      // has fully initialized the selected driver.
+    }).catch((error) => {
+      // "No available storage method found."
+      // One of the cases that `ready()` rejects,
+      // is when no usable storage driver is found
+      // Ensure we catch this error, but still log out other errors.
+      if (error.message !== 'No available storage method found.') {
+        console.log(error)
+      }
+    })
+    /* eslint-enable */
   }
 
   // Set the key and value in the indexedDB
