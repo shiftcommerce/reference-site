b3c91faa72e6a989e54983a270e6125e2f36887f
Callum Barratt <callum@barratt.me>
2018-11-19 13:44:55 +0000

    Fix the script tag duplicating on navigate (#477)
    * Fix the script tag duplicating on navigate
    
    * Fix the script tag duplicating on navigate
    
    * Move stripe script to checkout page
    
    * Single quotes
    
    * Keep stripe in the customhead

--- packages/reference/client/components/custom-head.js
+++ packages/reference/client/components/custom-head.js
@@ -3,11 +3,13 @@ import { Component } from 'react'
 
 class CustomHead extends Component {
   render () {
-    return <Head>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <script src='https://js.stripe.com/v3/' />
-      <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon.png' />
-    </Head>
+    return (
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon.png' key='favicon' />
+        <script src='https://js.stripe.com/v3/' key='stripe' />
+      </Head>
+    )
   }
 }
 
