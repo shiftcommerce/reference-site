856709a4a0e286ca1b91a73e8dbf4633f695bac6
Hubert Pompecki <hpompecki@gmail.com>
2019-03-29 10:47:20 +0000

    Enable filtering in the search box (#855)

--- packages/reference/client/pages/search.js
+++ packages/reference/client/pages/search.js
@@ -4,4 +4,8 @@ import { connect } from 'react-redux'
 // Pages
 import { algoliaReduxWrapper, SearchPage } from '@shiftcommerce/shift-next'
 
-export default algoliaReduxWrapper(connect()(SearchPage), SearchPage)
+const mapStateToProps = ({ search }) => {
+  return { search }
+}
+
+export default algoliaReduxWrapper(connect(mapStateToProps)(SearchPage), SearchPage)
