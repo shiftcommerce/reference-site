23c174eab37fe369f7ff118c7659feb9d96bd4b9
Hubert Pompecki <hpompecki@gmail.com>
2019-04-25 11:11:33 +0100

    Rogue redirects to search (#894)
    * Update shift-next
    
    * Update lockfile
    
    * Upgrade shift-next version pulled from github
    
    * Add cypress spec for testing navigation between static pages
    
    * Update shift-next
    
    * Increase timeout for api requests
    
    * Add spec for navigating around the my account page
    
    * Increase timeouts
    
    * Don't record videos in CI
    
    * Remove empty line
    
    * Remove trailing spaces

--- packages/reference/cypress/integration/home-page.js
+++
@@ -1,8 +0,0 @@
-context('Visit the reference site', () => {
-  beforeEach(() => {
-    cy.viewport(1200, 800)
-    cy.visit('/')
-  })
-
-  it('Is on the page', () => {})
-})
---
+++ packages/reference/cypress/integration/static-pages.js
@@ -0,0 +1,29 @@
+describe('Static pages', () => {
+  it('Navigates from one static page to another', () => {
+    // Start a mock server so we can wait for static page requests
+    cy.server()
+    cy.route('GET', '/getStaticPage/*').as('getStaticPage')
+
+    cy.visit('/')
+
+    // Check the home page was loaded
+    cy.wait('@getStaticPage')
+    cy.contains(/Test homepage heading/i)
+
+    // Go to another static page using the menu
+    cy.get('.o-nav')
+      .filter('.u-visible-d')
+      .contains(/Mobiles/i)
+      .click()
+
+    // Check the new static page was loaded
+    cy.wait('@getStaticPage')
+    cy.contains(/Explore our selection of mobile phones/i)
+
+    // Click the logo in the header
+    cy.get('.o-header__logo').click()
+
+    // Check the home page was loaded
+    cy.contains(/Test homepage heading/i)
+  })
+})
