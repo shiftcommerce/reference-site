edd70c865b76e6c99aac19918d3faf0f3e5f784f
Callum Barratt <callum@barratt.me>
2019-05-04 16:22:21 +0100

    Remove more dead code

--- packages/reference/client/requests/account-request.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../actions/action-types'
-
-export const accountRequest = {
-  endpoint: '/getAccount',
-  query: {},
-  errorActionType: types.ERROR_ACCOUNT,
-  requestActionType: types.GET_ACCOUNT,
-  successActionType: types.SET_ACCOUNT
-}
--- packages/reference/client/requests/customer-orders-request.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../actions/action-types'
-
-export const customerOrdersRequest = () => {
-  return {
-    endpoint: `/customerOrders`,
-    requestActionType: types.GET_CUSTOMER_ORDERS,
-    successActionType: types.SET_CUSTOMER_ORDERS
-  }
-}
