350720c5268b43e6e77ab6590ebd0f216019821a
Arron Fletcher <arron.fletcher@shiftcommerce.com>
2019-05-29 15:05:06 +0100

    moved product request query to handler

--- packages/shift-next/src/actions/product-actions.js
+++ packages/shift-next/src/actions/product-actions.js
@@ -5,15 +5,13 @@ import * as types from './action-types'
 const productRequest = (productId) => {
   return {
     endpoint: `/getProduct/${productId}`,
-    query: {
-      include: 'asset_files,variants,bundles,bundles.asset_files,template,meta.*',
-      fields: { asset_files: 'image_height,image_width,s3_url' }
-    },
     requestActionType: types.GET_PRODUCT,
     successActionType: types.SET_PRODUCT
   }
 }
 
 export function readProduct (productId) {
-  return readEndpoint(productRequest(productId))
+  return (dispatch) => {
+    return dispatch(readEndpoint(productRequest(productId)))
+  }
 }
