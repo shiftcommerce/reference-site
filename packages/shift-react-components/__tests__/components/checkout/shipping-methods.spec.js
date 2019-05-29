f05d5df3a1f835ab811d4d1965bb8c65c3a461bf
Callum Barratt <callum@barratt.me>
2019-05-29 12:16:29 +0100

    Fix up the shipping method spec

--- packages/shift-react-components/__tests__/components/checkout/shipping-methods.spec.js
+++ packages/shift-react-components/__tests__/components/checkout/shipping-methods.spec.js
@@ -14,13 +14,15 @@ import shippingMethodsFixture from '../../fixtures/shipping-methods'
 
 describe('ShippingMethods', () => {
   // Setup mock date for testing business days
-  MockDate.set('5/20/2019')
+  beforeEach(() => {
+    MockDate.set('5/20/2019')
+  })
 
   afterAll(() => {
     MockDate.reset()
   })
 
-  test('renders a nothing when shipping methods are loading', () => {
+  test('renders nothing when shipping methods are loading', () => {
     const wrapper = shallow(<ShippingMethods />)
 
     expect(wrapper.equals(null)).toBe(true)
@@ -62,71 +64,72 @@ describe('ShippingMethods', () => {
     // Check the handler has been called correctly
     expect(handleFormSubmit).toHaveBeenCalled()
   })
-})
 
-test('renders Continue To Payment button when card payments are made', () => {
-  const handleFormSubmit = jest.fn()
-  const handleSetShippingMethod = jest.fn()
-  const cartShippingMethod = cartFixture.shipping_method
-  const cartLineItemsCount = cartFixture.line_items_count
+  test('renders Continue To Payment button when card payments are made', () => {
+    const handleFormSubmit = jest.fn()
+    const handleSetShippingMethod = jest.fn()
+    const cartShippingMethod = cartFixture.shipping_method
+    const cartLineItemsCount = cartFixture.line_items_count
 
-  const wrapper = mount(<ShippingMethods
-    cartShippingMethod={cartShippingMethod}
-    cartLineItemsCount={cartLineItemsCount}
-    handleFormSubmit={handleFormSubmit}
-    handleSetShippingMethod={handleSetShippingMethod}
-    shippingMethods={shippingMethodsFixture}
-    isThirdPartyPayment={false}
-  />)
+    const wrapper = mount(<ShippingMethods
+      cartShippingMethod={cartShippingMethod}
+      cartLineItemsCount={cartLineItemsCount}
+      handleFormSubmit={handleFormSubmit}
+      handleSetShippingMethod={handleSetShippingMethod}
+      shippingMethods={shippingMethodsFixture}
+      isThirdPartyPayment={false}
+    />)
 
-  // Basic checks for classnames and content
-  expect(wrapper).toIncludeText(shippingMethodsFixture[0].label)
-  // Check for estimated delivery
-  expect(wrapper.find({ role: 'button' }).first()).toIncludeText('Continue To Payment')
-})
+    // Basic checks for classnames and content
+    expect(wrapper).toIncludeText(shippingMethodsFixture[0].label)
+    // Check for estimated delivery
+    expect(wrapper.find({ role: 'button' }).first()).toIncludeText('Continue To Payment')
+  })
 
-test('renders Review Your Order button when third party payment is made', () => {
-  const handleFormSubmit = jest.fn()
-  const handleSetShippingMethod = jest.fn()
-  const cartShippingMethod = cartFixture.shipping_method
-  const cartLineItemsCount = cartFixture.line_items_count
+  test('renders Review Your Order button when third party payment is made', () => {
+    const handleFormSubmit = jest.fn()
+    const handleSetShippingMethod = jest.fn()
+    const cartShippingMethod = cartFixture.shipping_method
+    const cartLineItemsCount = cartFixture.line_items_count
 
-  const wrapper = mount(<ShippingMethods
-    cartShippingMethod={cartShippingMethod}
-    cartLineItemsCount={cartLineItemsCount}
-    handleFormSubmit={handleFormSubmit}
-    handleSetShippingMethod={handleSetShippingMethod}
-    shippingMethods={shippingMethodsFixture}
-    isThirdPartyPayment={true}
-  />)
+    const wrapper = mount(<ShippingMethods
+      cartShippingMethod={cartShippingMethod}
+      cartLineItemsCount={cartLineItemsCount}
+      handleFormSubmit={handleFormSubmit}
+      handleSetShippingMethod={handleSetShippingMethod}
+      shippingMethods={shippingMethodsFixture}
+      isThirdPartyPayment={true}
+    />)
 
-  // Basic checks for classnames and content
-  expect(wrapper).toIncludeText(shippingMethodsFixture[0].label)
-  // Check for estimated delivery
-  expect(wrapper.find({ role: 'button' }).first()).toIncludeText('Review Your Order')
-})
+    // Basic checks for classnames and content
+    expect(wrapper).toIncludeText(shippingMethodsFixture[0].label)
+    // Check for estimated delivery
+    expect(wrapper.find({ role: 'button' }).first()).toIncludeText('Review Your Order')
+  })
 
-test('renders error messages', () => {
-  // Arrange
-  const errorMessage = 'Something went wrong'
-  const handleFormSubmit = jest.fn()
-  const handleSetShippingMethod = jest.fn()
-  const cartShippingMethod = cartFixture.shipping_method
-  const cartLineItemsCount = cartFixture.line_items_count
+  test('renders an error message', () => {
+    // Arrange
+    const errorMessage = 'Something went wrong'
+    const handleFormSubmit = jest.fn()
+    const handleSetShippingMethod = jest.fn()
+    const cartShippingMethod = cartFixture.shipping_method
+    const cartLineItemsCount = cartFixture.line_items_count
 
-  // Act
-  const wrapper = mount(<ShippingMethods
-    cartShippingMethod={cartShippingMethod}
-    cartLineItemsCount={cartLineItemsCount}
-    handleFormSubmit={handleFormSubmit}
-    handleSetShippingMethod={handleSetShippingMethod}
-    shippingMethods={shippingMethodsFixture}
-    isThirdPartyPayment={true}
-    errorMessage={errorMessage}
-  />)
+    // Act
+    const wrapper = mount(<ShippingMethods
+      cartShippingMethod={cartShippingMethod}
+      cartLineItemsCount={cartLineItemsCount}
+      handleFormSubmit={handleFormSubmit}
+      handleSetShippingMethod={handleSetShippingMethod}
+      shippingMethods={shippingMethodsFixture}
+      isThirdPartyPayment={true}
+      errorMessage={errorMessage}
+    />)
 
-  // Assert
-  expect(wrapper).toMatchSnapshot()
-  expect(wrapper.find(Flash).length).toEqual(1)
-  expect(wrapper).toIncludeText(errorMessage)
+    // Assert
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toIncludeText('Tuesday 21st May')
+    expect(wrapper.find(Flash).length).toEqual(1)
+    expect(wrapper).toIncludeText(errorMessage)
+  })
 })
