e02484804862056cb84cdd89c0d8558c9c186228
Arron Fletcher <arron.fletcher@shiftcommerce.com>
2019-05-29 16:18:51 +0100

    added fields and includes to category query

--- packages/shift-next-routes/src/express/category-handler.js
+++ packages/shift-next-routes/src/express/category-handler.js
@@ -1,8 +1,19 @@
 const { SHIFTClient } = require('@shiftcommerce/shift-node-api')
 const { setSurrogateHeaders } = require('../lib/set-cache-headers')
 
+const categoryApiEndpointQuery = {
+  fields: {
+    asset_files: 's3_url,canonical_path',
+    products: 'title,description,slug,canonical_path,reference,asset_files,min_price,max_price,min_current_price,max_current_price,rating,meta_attributes,updated_at',
+    variants: 'price,meta_attributes,sku,asset_files',
+    category_trees: 'path,slug'
+  },
+  include: 'asset_files,variants,bundles'
+}
+
 module.exports = {
   getCategory: async (req, res) => {
+    req.query = { ...req.query, ...categoryApiEndpointQuery }
     const response = await SHIFTClient.getCategoryV1(req.params.id, req.query)
 
     setSurrogateHeaders(response.headers, res)
