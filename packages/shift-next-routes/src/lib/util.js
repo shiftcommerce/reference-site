d4937ccee9a37b97c3b3ab5719c2ab70aed5391d
Marc Gear <marcgear@gmail.com>
2019-05-31 16:50:16 +0100

    fix tests

--- packages/shift-next-routes/src/lib/util.js
+++ packages/shift-next-routes/src/lib/util.js
@@ -2,7 +2,7 @@
  * are we in a secure environment or not
  */
 const isSecure = () => {
-  return process.env.NO_HTTPS ? false : true
+  return process.env.NO_HTTPS !== true
 }
 
 module.exports = { isSecure }
--- packages/shift-next-routes/test/lib/util.test.js
+++ packages/shift-next-routes/test/lib/util.test.js
@@ -1,9 +1,25 @@
-const { isSecure } = require('../../lib/util')
+const { isSecure } = require('../../src/lib/util')
 
-test('isSecure', () => {
-  process.env.NO_HTTPS = false
-  expect(isSecure()).toBe(true)
+describe('test isSecure', () => {
+  const OLD_ENV = process.env;
 
-  process.env.NO_HTTPS = 'true'
-  expect(isSecure()).toBe(false)
+  beforeEach(() => {
+    jest.resetModules() // this is important - it clears the cache
+    process.env = { ...OLD_ENV };
+    delete process.env.NO_HTTPS;
+  })
+
+  afterEach(() => {
+    process.env = OLD_ENV;
+  })
+
+  test('false when not secure', () => {
+    process.env.NO_HTTPS = true
+    expect(isSecure()).toEqual(false)
+  })
+
+  test('true when secure', () => {
+    process.env.NO_HTTPS = false
+    expect(isSecure()).toEqual(true)
+  })
 })
