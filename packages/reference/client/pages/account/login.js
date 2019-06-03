6c6430542d686c6f7d7c5d505257c7f55fa1d48c
Callum Barratt <callum@barratt.me>
2019-06-03 16:00:45 +0100

    Revert "updated login page to pass more props to login form"
    This reverts commit 980a4dc72a583603a121fd1c91b7860532825b83.

--- packages/reference/client/pages/account/login.js
+++ packages/reference/client/pages/account/login.js
@@ -4,19 +4,8 @@ import { connect } from 'react-redux'
 import { algoliaReduxWrapper } from '@shiftcommerce/shift-next/src/lib/algolia-redux-wrapper'
 import LoginPage from '@shiftcommerce/shift-next/src/pages/login'
 
-const formTranslations = {
-  title: 'login',
-  caption: 'Please enter your details below',
-  loginButtonText: 'continue securely',
-  registerButtonText: 'create new account',
-  emailInputLabel: 'Email *',
-  emailPlaceholder: 'Email',
-  passwordInputLabel: 'Password *',
-  passwordPlaceholder: 'Password'
-}
-
 function mapStateToProps ({ login, account: { loggedIn } }) {
-  return { login, loggedIn, formTranslations }
+  return { login, loggedIn }
 }
 
 export default algoliaReduxWrapper(connect(mapStateToProps)(LoginPage), LoginPage)
--- packages/shift-react-components/src/components/account/login-form.js
+++ packages/shift-react-components/src/components/account/login-form.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
-import Link from 'next/link'
 
 // lib
 import { Button } from '../../objects/button'
@@ -12,20 +11,11 @@ import { FormErrors } from '../../lib/form-errors'
 
 export class LoginForm extends Component {
   renderEmailInputField () {
-    const { emailInputLabel, emailPlaceholder } = this.props.formTranslations
-
     return (
       <div className='o-flex o-flex__space-between'>
         <div className='o-flex-full-width-s'>
-          <label htmlFor='login-email' className='o-form__label'><b>{ emailInputLabel }</b></label>
-          <Field
-            autoComplete='username'
-            className='o-form__input-field o-form__input-block'
-            id='login-email'
-            name='email'
-            placeholder={emailPlaceholder}
-            type='email'
-          />
+          <label className='o-form__label'><b>Email *</b></label>
+          <Field type='email' name='email' placeholder='Email' className='o-form__input-field o-form__input-block' />
           <div className='o-form__input-field__error'>
             <ErrorMessage name='email' />
           </div>
@@ -35,20 +25,11 @@ export class LoginForm extends Component {
   }
 
   renderPasswordInputField () {
-    const { passwordInputLabel, passwordPlaceholder } = this.props.formTranslations
-
     return (
       <div className='o-flex o-flex__space-between'>
         <div className='o-flex-full-width-s'>
-          <label htmlFor='login-password' className='o-form__label'><b>{ passwordInputLabel }</b></label>
-          <Field
-            autoComplete='current-password'
-            className='o-form__input-field o-form__input-block'
-            id='login-password'
-            name='password'
-            placeholder={passwordPlaceholder}
-            type='password'
-          />
+          <label className='o-form__label'><b>Password *</b></label>
+          <Field type='password' name='password' placeholder='Password' className='o-form__input-field o-form__input-block' />
           <div className='o-form__input-field__error'>
             <ErrorMessage name='password' />
           </div>
@@ -58,13 +39,11 @@ export class LoginForm extends Component {
   }
 
   renderSubmitButton (props) {
-    const { loginButtonText } = this.props.formTranslations
-
     return (
       <Button
         className='c-login__button o-button--sml'
         aria-label='Continue Securely'
-        label={ loginButtonText }
+        label='CONTINUE SECURELY'
         status={(props.isValid ? 'positive' : 'disabled')}
         type='submit'
         disabled={!props.isValid}
@@ -97,8 +76,7 @@ export class LoginForm extends Component {
     const {
       className,
       handleSubmit,
-      login,
-      formTranslations
+      login
     } = this.props
 
     const initialValues = {
@@ -115,17 +93,8 @@ export class LoginForm extends Component {
     })
 
     return (
-      <div className ='c-login'>
-        <h1 className='c-login__title'>{ formTranslations.title }</h1>
-        <p className='c-login__caption'>{ formTranslations.caption }</p>
-        <div className={classNames('o-form', className)}>
-          { this.renderFormik(initialValues, loginSchema, handleSubmit, login) }
-        </div>
-        <a href='/account/forgotpassword' className='c-login__anchor'>Reset Password?</a>
-        <p className='c-login__caption'>Don't have an account?</p>
-        <Link href='/account/register'>
-          <a className='c-login__register-button'>{ formTranslations.registerButtonText }</a>
-        </Link>
+      <div className={classNames('o-form', className)}>
+        { this.renderFormik(initialValues, loginSchema, handleSubmit, login) }
       </div>
     )
   }
