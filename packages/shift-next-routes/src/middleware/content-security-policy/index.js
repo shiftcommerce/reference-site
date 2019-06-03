b5b7ac139be549c50b56307545be377fcff86560
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-06-03 16:02:22 +0100

    Add section CSPs -- WIP

--- packages/shift-next-routes/src/middleware/content-security-policy/index.js
+++ packages/shift-next-routes/src/middleware/content-security-policy/index.js
@@ -17,27 +17,16 @@ const { staticPageContentSecurityPolicy } = require('./sections/static-page')
  * @param {object} options  - eg. imageHosts, styleHosts, scriptHosts, frameHosts, connectHosts
  */
 const contentSecurityPolicy = (server, options = {}) => {
-  // Set MyAccount CSP
   accountPageContentSecurityPolicy(server, options)
-  // Set Cart page CSP
   cartPageContentSecurityPolicy(server, options)
-  // Set PLP CSP
   categoryPageContentSecurityPolicy(server, options)
-  // Set Checkout CSP
   checkoutContentSecurityPolicy(server, options)
-  // Set Forgot Password page CSP
   forgotPasswordPageContentSecurityPolicy(server, options)
-  // Set Login page CSP
   loginPageContentSecurityPolicy(server, options)
-  // Set Order page CSP
   orderPageContentSecurityPolicy(server, options)
-  // Set PDP CSP
   productPageContentSecurityPolicy(server, options)
-  // Set Registration page CSP
   registrationPageContentSecurityPolicy(server, options)
-  // Set Search page CSP
   searchPageContentSecurityPolicy(server, options)
-  // Set static page CSP
   staticPageContentSecurityPolicy(server, options)
 }
 
