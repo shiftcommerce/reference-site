30dea4e66c8699a4fa1ec7de03f1fd200e2416e6
Marc Gear <marcgear@gmail.com>
2019-06-06 12:23:50 +0100

    reverse changes to this - WHY WERE THEY MADE???

--- packages/reference/next.config.js
+++ packages/reference/next.config.js
@@ -1,14 +1,36 @@
 const path = require('path')
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
+const nextRuntimeDotenv = require('next-runtime-dotenv')
 const withTM = require('next-transpile-modules')
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const withConfig = nextRuntimeDotenv({
+  public: [
+    'API_HOST_PROXY',
+    'ALGOLIA_APP_ID',
+    'ALGOLIA_API_KEY',
+    'ALGOLIA_INDEX_NAME',
+    'ALGOLIA_RESULTS_PER_PAGE',
+    'STRIPE_API_KEY',
+    'PAYPAL_CLIENT_ID',
+    'ENABLE_TEST_PAYPAL_BUTTON'
+  ],
+  server: [
+    'API_TENANT',
+    'API_ACCESS_TOKEN',
+    'API_HOST',
+    'OMS_HOST',
+    'SESSION_SECRET',
+    'IMAGE_HOSTS',
+    'SCRIPT_HOSTS',
+    'SECRET_STRIPE_API_KEY',
+    'PAYPAL_CLIENT_SECRET',
+    'ENABLE_PAYPAL_LIVE_ENVIRONMENT'
+  ]
+})
 
-module.exports = withBundleAnalyzer(withCSS(withSass(withTM({
+module.exports = withConfig(withBundleAnalyzer(withCSS(withSass(withTM({
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
@@ -30,33 +52,6 @@ module.exports = withBundleAnalyzer(withCSS(withSass(withTM({
     '@shiftcommerce/shift-react-components',
     '@shiftcommerce/shift-node-api'
   ],
-
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    API_TENANT: process.env.API_TENANT,
-    API_ACCESS_TOKEN: process.env.API_ACCESS_TOKEN,
-    API_HOST: process.env.API_HOST,
-    OMS_HOST: process.env.OMS_HOST,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    IMAGE_HOSTS: process.env.IMAGE_HOSTS,
-    SCRIPT_HOSTS: process.env.SCRIPT_HOSTS,
-    SECRET_STRIPE_API_KEY: process.env.SECRET_STRIPE_API_KEY,
-    LOG_LEVEL: process.env.LOG_LEVEL
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    API_HOST_PROXY: process.env.API_HOST_PROXY,
-    ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
-    ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
-    ALGOLIA_INDEX_NAME: process.env.ALGOLIA_INDEX_NAME,
-    ALGOLIA_RESULTS_PER_PAGE: process.env.ALGOLIA_RESULTS_PER_PAGE,
-    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-    IMGIX_SOURCE: process.env.IMGIX_SOURCE,
-    IMGIX_HOSTNAMES_TO_REPLACE: process.env.IMGIX_HOSTNAMES_TO_REPLACE,
-    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-    GOOGLE_TAG_MANAGER_ID: process.env.GOOGLE_TAG_MANAGER_ID
-  },
-
   poweredByHeader: false,
   assetPrefix: process.env.ASSET_HOST || '',
   onDemandEntries: {
@@ -90,4 +85,4 @@ module.exports = withBundleAnalyzer(withCSS(withSass(withTM({
 
     return config
   }
-}))))
+})))))
