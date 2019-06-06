e1bf76feca092704c88de2c57dce5d991aac1e25
Callum Barratt <callum@barratt.me>
2019-06-06 10:58:25 +0100

    Fix versions

--- packages/shift-react-components/__tests__/support/enzyme-setup.js
+++ packages/shift-react-components/__tests__/support/enzyme-setup.js
@@ -2,6 +2,7 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
 window.matchMedia =
   window.matchMedia ||
   function () {
