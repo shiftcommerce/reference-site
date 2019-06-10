a8b539eb4de19719df892a85ddeb7a9d2d64ac4f
Marc Gear <marcgear@gmail.com>
2019-06-10 15:37:34 +0100

    add test cases

--- packages/shift-next/test/reducers/set-cart.test.js
+++ packages/shift-next/test/reducers/set-cart.test.js
@@ -1,21 +1,37 @@
 import setCart from '../../src/reducers/set-cart'
 import * as actionTypes from '../../src/actions/action-types'
 
+test('sets initial State', () => {
+  expect(setCart(undefined, { type: 'ANYTHING' })).toEqual({ miniBagDisplayed: false })
+})
+
 test('returns an empty cart when SET_ORDER is dispatched', () => {
   expect(setCart({ key: 'value' }, { type: actionTypes.SET_ORDER })).toEqual({})
 })
 
-test('returns an updated cart when CART_UPDATED is dispatched', () => {
+test('returns an updated cart when CART_UPDATED is dispatched and sums quantity correctly', () => {
   const action = {
     type: actionTypes.CART_UPDATED,
     payload: {
-      key: 'new value'
+      key: 'new value',
+      line_items: [
+        { unit_quantity: 6 },
+        { unit_quantity: 4 }
+      ],
+      line_items_count: 2
     }
   }
 
-  expect(setCart({ key: 'value' }, action)).toEqual({
-    key: 'new value'
-  })
+  const expected = {
+    key: 'new value',
+    line_items: [
+      { unit_quantity: 6 },
+      { unit_quantity: 4 }
+    ],
+    line_items_count: 10
+  }
+
+  expect(setCart({ key: 'value' }, action)).toEqual(expected)
 })
 
 test("doesn't change the state when an unrecognized action is dispatched", () => {
