c86e81e17967c08fb6141f704d19ae1a8faf9ca9
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-06-11 09:28:25 +0100

    Share state for Review and submit page specs

--- packages/reference/cypress/integration/checkout/paypal/review_and_submit_page_spec.js
+++ packages/reference/cypress/integration/checkout/paypal/review_and_submit_page_spec.js
@@ -1,19 +1,22 @@
 describe('Checkout', () => {
   describe('PayPal', () => {
     context('Navigating To Review & Submit Page', () => {
-      beforeEach(() => {
+      it('renders the page correctly and allows customers to change shipping method', () => {
         // Navigate to Review & Submit Checkout Page
         // Uses custom command - Cypress/support/commands/checkout.js
         cy.navigateToReviewCheckoutPage()
-      })
-
-      it('renders the page correctly', () => {
         // Check Payment Method section header is present
         cy.get('.c-payment-method__header').contains(/Payment Method/i)
         // Check Shipping Address section header is present
         cy.get('.c-address-form__header').contains(/Shipping Address/i)
+        // Check Shipping Address Edit button is not present
+        cy.get('.c-address-form__header').find(/Edit/i).should('not.exist')
         // Check Shipping Method section header is present
         cy.get('.c-shipping-method__header').contains(/Shipping Method/i)
+        // CheckPayment Details section header is present
+        cy.get('.c-payment__header').contains(/Payment Details/i)
+        // Check Payment Details Edit button is not present
+        cy.get('.c-payment__header').find(/Edit/i).should('not.exist')
         // Check that the shopping basket is not empty
         cy.contains(/You have 1 item in your shopping basket/i)
         // Check the line item in the shopping basket
@@ -24,28 +27,6 @@ describe('Checkout', () => {
         cy.get('.c-line-items__quantity').find('.c-line-items__quantity-select').should('not.exist')
         // Check it renders the line item quantity
         cy.get('.c-line-items__quantity').find('.c-line-items__quantity-amount').should('exist')
-      })
-
-      it('allows customers to change payment method', () => {
-        // Check Paym`ent Method Edit button is present and Navigate to the Shipping Method Page
-        cy.get('.c-payment-method__header').contains(/Edit/i).click()
-        // Check we have been redirected to the Payment Method Page
-        cy.url().should('includes', '/checkout/payment-method')
-        // Wait for PayPal script and page to be loaded
-        cy.wait(3000)
-        // Check that the PayPal button is present
-        cy.get('.paypal-buttons-context-iframe').contains('iframe')
-        // Choose credit card for payment method
-        cy.contains(/Pay by credit\/debit card/i).click()
-        // Check its the Shipping Address Page
-        cy.url().should('includes', '/checkout/shipping-address')
-        // Check Shipping Address section header is present
-        cy.get('.c-address-form__header').contains(/Shipping Address/i)
-        // Check the correct Payment Method is displayed
-        cy.get('.c-payment-method__summary').contains('Credit/Debit Card')
-      })
-
-      it('allows customers to change shipping method', () => {
         // Stub update shipping method request
         cy.route({
           method: 'POST',
@@ -67,17 +48,6 @@ describe('Checkout', () => {
         })
         // Check its the Review Page Page
         cy.url().should('includes', '/checkout/review')
-        cy.contains(/Place Order/i)
-      })
-
-      it('does not allow customers to change shipping address', () => {
-        // Check Shipping Address Edit button is not present
-        cy.get('.c-address-form__header').find(/Edit/i).should('not.exist')
-      })
-
-      it('does not allow customers to change payment details', () => {
-        // Check Shipping Address Edit button is not present
-        cy.get('.c-payment__header').find(/Edit/i).should('not.exist')
       })
 
       it('displays an error when the PayPal order cannot be authorized', () => {
@@ -163,6 +133,25 @@ describe('Checkout', () => {
 
         // Check error message is rendered
         cy.contains(/Invalid promo code TESSSSSSSTCOUPONSSSSS/i)
+      })
+
+      it('allows customers to change payment method', () => {
+        // Check Paym`ent Method Edit button is present and Navigate to the Shipping Method Page
+        cy.get('.c-payment-method__header').contains(/Edit/i).click()
+        // Check we have been redirected to the Payment Method Page
+        cy.url().should('includes', '/checkout/payment-method')
+        // Wait for PayPal script and page to be loaded
+        cy.wait(2500)
+        // Check that the PayPal button is present
+        cy.get('.paypal-buttons-context-iframe').contains('iframe')
+        // Choose credit card for payment method
+        cy.contains(/Pay by credit\/debit card/i).click()
+        // Check its the Shipping Address Page
+        cy.url().should('includes', '/checkout/shipping-address')
+        // Check Shipping Address section header is present
+        cy.get('.c-address-form__header').contains(/Shipping Address/i)
+        // Check the correct Payment Method is displayed
+        cy.get('.c-payment-method__summary').contains('Credit/Debit Card')
       })
     })
   })
