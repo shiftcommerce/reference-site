de1b74b59612491e2cba90ffde4e606c6d951b3a
Owen Smith <ozz1649@gmail.com>
2019-06-17 14:25:06 +0100

    fix pt3

--- packages/shift-next/test/support/global-setup.js
+++ packages/shift-next/test/support/global-setup.js
@@ -2,4 +2,5 @@ module.exports = async (_globalConfig) => {
   // Environment variables expected by the package
   process.env.API_HOST = 'http://example.com'
   process.env.API_TENANT = 'test_tenant'
+  process.env.PAYPAL_CLIENT_ID = 'test'
 }
