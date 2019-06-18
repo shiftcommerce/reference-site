9edc3c006cb11b12bd0b95836e61459505be17c5
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-06-18 12:16:52 +0100

    Document Memcached client lib configs

--- packages/shift-next-routes/src/lib/memcached-client.js
+++ packages/shift-next-routes/src/lib/memcached-client.js
@@ -2,18 +2,23 @@
 const Memcached = require('memcache-client')
 
 // Config
+// fetch the available memcached servers
 const memcachedServers = (process.env.MEMCACHED_SERVERS || '').split(',')
+// configure multiple redundant servers support
+const memcachedServerConfigs = memcachedServers.map((server) => {
+  // set max connections for each server
+  return { server: server, maxConnections: 5 }
+})
+// build the memcached config
 const memcachedConfig = {
   server: {
-    servers: memcachedServers.map((server) => {
-      return { server: server, maxConnections: 5 }
-    }),
+    servers: memcachedServerConfigs, // setup multiple redundant servers
     config: {
       retryFailedServerInterval: 1000, // (ms) - how often to check failed servers
       failedServerOutTime: 30000 // (ms) how long a failed server should be out before retrying it
     }
   },
-  cmdTimeout: 1500 // (ms) command timeout
+  cmdTimeout: 1500 //(ms) connection is shutdown and Error returned if a response is not received before the timeout value
 }
 
 /**
