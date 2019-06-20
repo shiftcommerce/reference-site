7f79a04978d1dec0864bde10989a445763707f4c
Callum Barratt <callum@barratt.me>
2019-06-20 09:28:02 +0100

    Fix account sidebar link (#40)
    * Wrap the whole li with the link
    
    * Remove empty space

--- packages/shift-react-components/src/components/account/sidebar-item.js
+++ packages/shift-react-components/src/components/account/sidebar-item.js
@@ -5,10 +5,14 @@ import classNames from 'classnames'
 import Config from '../../lib/config'
 import link from '../../objects/link'
 
-const Link = Config.get().Link || link
-
 export const SidebarItem = ({ label, location, current }) => {
-  return <li className={classNames('c-sidebar__item', { 'c-sidebar__item--current': current })}>
-    <Link href={location}>{ label }</Link>
-  </li>
+  const Link = Config.get().Link || link
+
+  return (
+    <Link href={location}>
+      <li className={classNames('c-sidebar__item', { 'c-sidebar__item--current': current })}>
+        { label }
+      </li>
+    </Link>
+  )
 }
