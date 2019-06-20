110c65e106773afec56a9908eb306717ac2e39b9
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-06-20 11:21:46 +0100

    Update env vars and add debugging logic for QA on heroku

--- packages/shift-next-routes/src/lib/memcached-client.js
+++ packages/shift-next-routes/src/lib/memcached-client.js
@@ -2,7 +2,7 @@
 const memjs = require('memjs')
 
 // Config
-const memcachedServers = (process.env.MEMCACHE_SERVERS || process.env.MEMCACHEDCLOUD_SERVERS)
+const memcachedServers = (process.env.MEMCACHE_SERVERS || process.env.MEMCACHIER_SERVERS || process.env.MEMCACHEDCLOUD_SERVERS)
 const username = (process.env.MEMCACHE_USERNAME || process.env.MEMCACHEDCLOUD_USERNAME)
 const password = (process.env.MEMCACHE_PASSWORD || process.env.MEMCACHEDCLOUD_PASSWORD)
 
