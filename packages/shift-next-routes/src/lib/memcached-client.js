54b474e0a5b417e93b3f7bcfd3d9209203889006
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-06-20 11:30:29 +0100

    Update env vars to include memcachier

--- packages/shift-next-routes/src/lib/memcached-client.js
+++ packages/shift-next-routes/src/lib/memcached-client.js
@@ -1,10 +1,11 @@
 // Library
+// @TODO - update when security vulnerability is addressed https://github.com/memcachier/memjs/issues/123
 const memjs = require('memjs')
 
 // Config
 const memcachedServers = (process.env.MEMCACHE_SERVERS || process.env.MEMCACHIER_SERVERS || process.env.MEMCACHEDCLOUD_SERVERS)
-const username = (process.env.MEMCACHE_USERNAME || process.env.MEMCACHEDCLOUD_USERNAME)
-const password = (process.env.MEMCACHE_PASSWORD || process.env.MEMCACHEDCLOUD_PASSWORD)
+const username = (process.env.MEMCACHE_USERNAME || process.env.MEMCACHIER_USERNAME || process.env.MEMCACHEDCLOUD_USERNAME)
+const password = (process.env.MEMCACHE_PASSWORD || process.env.MEMCACHIER_PASSWORD || process.env.MEMCACHEDCLOUD_PASSWORD)
 
 /**
  *  MemcachedClient
