d514505d160d17d446b7f6d96614d9a331cfaecf
Callum Barratt <callum@barratt.me>
2019-06-24 15:41:23 +0100

    Remove unused code

--- packages/shift-react-components/src/components/account/sidebar-item.js
+++ packages/shift-react-components/src/components/account/sidebar-item.js
@@ -1,15 +1,9 @@
 // Libraries
 import React from 'react'
 import classNames from 'classnames'
-
-import Config from '../../lib/config'
-import link from '../../objects/link'
-
 import Router from 'next/router'
 
 export const SidebarItem = ({ label, location, current }) => {
-  const Link = Config.get().Link || link
-
   return (
     <li onClick={() => Router.push(location)} className={classNames('c-sidebar__item', { 'c-sidebar__item--current': current })}>
       { label }
