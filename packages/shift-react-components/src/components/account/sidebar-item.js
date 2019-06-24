2776370ba21fc89b6da3e060e9c22891e2e5364c
Callum Barratt <callum@barratt.me>
2019-06-24 15:37:00 +0100

    Use Router.push to avoid generating invalid elements

--- packages/shift-react-components/src/components/account/sidebar-item.js
+++ packages/shift-react-components/src/components/account/sidebar-item.js
@@ -5,14 +5,14 @@ import classNames from 'classnames'
 import Config from '../../lib/config'
 import link from '../../objects/link'
 
+import Router from 'next/router'
+
 export const SidebarItem = ({ label, location, current }) => {
   const Link = Config.get().Link || link
 
   return (
-    <Link href={location}>
-      <li className={classNames('c-sidebar__item', { 'c-sidebar__item--current': current })}>
-        { label }
-      </li>
-    </Link>
+    <li onClick={() => Router.push(location)} className={classNames('c-sidebar__item', { 'c-sidebar__item--current': current })}>
+      { label }
+    </li>
   )
 }
