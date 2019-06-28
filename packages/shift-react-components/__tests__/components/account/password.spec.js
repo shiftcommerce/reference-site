0033da0bc8f17e3c4b694009183fefbb77924f65
Arron Fletcher <arron.fletcher@shiftcommerce.com>
2019-06-28 09:40:26 +0100

    updated password component spec

--- packages/shift-react-components/__tests__/components/account/password.spec.js
+++ packages/shift-react-components/__tests__/components/account/password.spec.js
@@ -7,11 +7,24 @@ import { AccountPassword } from '../../../src/components/account/password'
 import { Button } from '../../../src/objects/button'
 
 test('renders the form', () => {
-  const wrapper = mount(<AccountPassword />)
+  // Arrange
+  const formOptions = {
+    title: {
+      visible: true,
+      translation: 'test title'
+    },
+    oldPasswordPlaceholder: 'placeholder 1',
+    newPasswordPlaceholder: 'placeholder 2',
+    confirmationPlaceholder: 'placeholder 3'
+  }
+
+  // Act
+  const wrapper = mount(<AccountPassword formOptions={formOptions} />)
 
   // Assert
   expect(wrapper).toMatchSnapshot()
   expect(wrapper.find(Formik)).toExist()
   expect(wrapper.find(Field).length).toEqual(3)
   expect(wrapper.find(Button)).toExist()
+  expect(wrapper.find('h1').at(0)).toHaveText('test title')
 })
